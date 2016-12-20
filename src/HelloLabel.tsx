4158c4543a61da973b8f330c7dec6584fb72f2a2
Günter Prossliner <Guenter.Prossliner@world-direct.at>
2016-12-20 10:06:41 +0100

    STEP4: Functional Component

---
+++ src/HelloLabel.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";     // we always need to import react
+export default ()=>                 // we just export a single function, which represents the "render" function
+
+<div>
+    Hello you!
+</div>
--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import HelloWorld from "./helloworld";
+import HelloLabel from "./HelloLabel";
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloLabel />,
   document.getElementById("root")
 );
